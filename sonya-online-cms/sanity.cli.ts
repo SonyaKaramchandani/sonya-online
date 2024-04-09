877c9a8bc68f89101a688714b70f2d4144d0b2ad
Sonya K <35080007+SonyaKaramchandani@users.noreply.github.com>
2024-04-09 14:17:39 -0400

    initalize sanity studio

---
+++ sonya-online-cms/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'p6g6ock4',
+    dataset: 'production'
+  }
+})
---
+++ sonya-online-cms/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sonya-online-cms',
+
+  projectId: 'p6g6ock4',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
