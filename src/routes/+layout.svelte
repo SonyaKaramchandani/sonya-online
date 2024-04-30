cfe51b294901f6bb464ce2bf1afc626068f2b1be
Sonya K <35080007+SonyaKaramchandani@users.noreply.github.com>
2024-04-30 00:26:53 -0400

    page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.css';
+	import { fade } from 'svelte/transition';
+
+	export let data;
+
+	$: pathname = data.pathname;
 </script>
 
-<slot />
+<!-- TODO: make page transitions more fun -->
+{#key pathname}
+	<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+		<slot />
+	</div>
+{/key}
